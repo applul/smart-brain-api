1092c6347314b4c24e69438df5c19966295f6baf
applul <ray.houston44@gmail.com>
2019-06-20 18:10:06 +0800

    much more work done

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.use(bodyParser.json());
 
 const database = {
     users: [
-        {
+        { 
             id: 123,
             name: "Holo",
             email: "Holo@Wolf-mail.com",
@@ -33,6 +33,10 @@ const database = {
     ]
 }
 
+app.get('/', (req, res) => {
+    res.send(database.users);
+})
+
 app.get("/", (req, res) => {
     res.send('this is working'); 
 })
@@ -41,9 +45,35 @@ app.post("/signin", (req, res) => {
     if (req.body.email === database.users[0].email && 
         req.body.password === database.users[0].password) {
             res.json('success!')
+            console.log("working here too")
         } else {
             res.status(400).json("Error logging in");
+            console.log('error, but working here too')
         }
+})
+
+app.post("/register", (req, res) => {
+    const { email, name, password } = req.body;
+    database.users.push({
+        id: 123, 
+        name: name,
+        email: email,
+        password: password,
+        entries: 0,
+        joined: new Date()
+    });
+    res.json(database.users[database.users.length - 1])
+})
+
+app.get("/profile/:id", (req, res) => {
+    const { id } = req.params;
+    database.users.forEach((user) => {
+        if (user.id === id) {
+            res.json(user);
+        } else {
+            res.status(404).json('no such user')
+        }
+    })
 })
 
 app. listen(3000, () => {
