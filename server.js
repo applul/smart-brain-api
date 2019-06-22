c9d20047ffcc585813346ecb4ed4a5b498f30e14
applul <ray.houston44@gmail.com>
2019-06-22 15:03:22 +0800

    npm installed bcrypt-nodejs

--- server.js
+++ server.js
@@ -100,6 +100,18 @@ app.put('/image', (req, res) => {
     loopUsers();
 })
 
+bcrypt.hash("bacon", null, null, function(err, hash) {
+    // Store hash in your password DB.
+});
+
+// Load hash from your password DB.
+bcrypt.compare("bacon", hash, function(err, res) {
+    // res == true
+});
+bcrypt.compare("veggies", hash, function(err, res) {
+    // res = false
+});
+
 app. listen(3000, () => { 
     console.log('app is running on port 3000');
 })
